eefcf4a51b67d383392cadc34b1d50e81842e89a
charlyecastro <castroc@uw.edu>
2019-12-03 22:45:26 -0800

    restructured directory

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/404.js"))),
+  "component---src-pages-beacon-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/beacon.js"))),
+  "component---src-pages-charade-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/charade.js"))),
+  "component---src-pages-earlybird-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/earlybird.js"))),
+  "component---src-pages-iemotion-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/iemotion.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/index.js"))),
+  "component---src-pages-limeade-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/limeade.js"))),
+  "component---src-pages-wordz-js": hot(preferDefault(require("/Users/Charles/Desktop/charlyecastro.github.io/src/pages/wordz.js")))
+}
+

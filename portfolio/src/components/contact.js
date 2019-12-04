cd81c1be4c1a0af62c1491dff4283b1b7a7f9129
charlyecastro <castroc@uw.edu>
2019-12-03 21:58:51 -0800

    set up form api & worked on 404 page

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -15,11 +15,11 @@ const Contact = () => {
                 <div className = "contained section-spacing" style ={{maxWidth: "800px", borderRadius : "5px", justifyContent : "center", alignSelf : "center"}}>
                 <div  className = "contactForm">
                 <h2 className = "sectionTitle contactTitle">Let's Talk</h2>
-                <form style = {{display : "flex", flexDirection : "column"}}>
+                <form style = {{display : "flex", flexDirection : "column"}} action="//formspree.io/charlyecastro@gmail.com" method="POST">
                     <label>Full Name</label>
                     <input type = "text" name = "name" placeholder = "Peter Parker"></input>
                     <label>Email</label>
-                    <input type = "text" name = "email" placeholder = "spidey@avengers.com"></input>
+                    <input type = "text" name="_replyto" placeholder = "spidey@avengers.com"></input>
                     <label>Message</label>
                     <textarea name="message" placeholder=" Hi Charlye" rows = {4}></textarea>
                     <input type="submit" value="Send"></input>
--- portfolio/src/pages/404.js
+++ portfolio/src/pages/404.js
@@ -2,8 +2,11 @@ import React from "react"
 
 const NotFoundPage = () => (
   <div id="errorPage">
-    <div className="contained" style={{ textAlign: "center" }}>
+    <div className="contained" style={{ textAlign: "center", justifyContent : "center" }}>
       <h1 style={{}}>404</h1>
+      <h3>Looks like youre lost</h3>
+      <h4>the page you are looking for can’t be found</h4>
+      <button > Let's Talk</button> 
     </div>
   </div>
 )

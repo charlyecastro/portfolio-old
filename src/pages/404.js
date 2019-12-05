8de5bb51c5fccb1ffa37d44efc3e9ff2339d6254
charlyecastro <castroc@uw.edu>
2019-12-04 16:59:27 -0800

    added favicon & nearly complete 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from "react"
+import {Link } from "gatsby"
 
 const NotFoundPage = () => (
   <div id="errorPage">
-    <div className="contained" style={{ textAlign: "center", justifyContent : "center" }}>
-      <h1 style={{}}>404</h1>
-      <h3>Looks like youre lost</h3>
-      <h4>the page you are looking for can’t be found</h4>
-      <button > Let's Talk</button> 
+    <div className="contained" style = {{alignSelf : "center"}}>
+      <h1 id = "errorTitle" style={{}}>404</h1>
+      <h2 style = {{color : "#FFFFFF", fontWeight : 500}}>Looks like you're lost</h2>
+      <h4 style = {{color : "#FFFFFF", fontWeight : 200}}>the page you are looking for can’t be found</h4>
+      <Link to= "/">
+      <button id = "errorBtn"  > go back</button> </Link>
     </div>
   </div>
 )
